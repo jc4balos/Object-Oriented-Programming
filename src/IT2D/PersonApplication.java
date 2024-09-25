48967f7071d6230c32e826302e57a4cc9bf98898
Jc Abalos <jc4balos@gmail.com>
2024-09-25 11:01:52 +0800

    added arrays

--- src/IT2D/PersonApplication.java
+++ src/IT2D/PersonApplication.java
@@ -3,17 +3,28 @@ package IT2D;
 public class PersonApplication {
     public static void main(String[] args) {
         //created an object
-        Person keni = new Person();
+//        Person keni = new Person();
+//
+//        //printed name atribute
+//        System.out.println(keni.name);
+//        keni.personInfo();
+//        keni.printInput("Baltazar", 21);
+//
+//        int x = 3;
+//        System.out.println(x);
+//
+//        Bag thisBag = new Bag();
+//        System.out.println(thisBag.ballpen);
 
-        //printed name atribute
-        System.out.println(keni.name);
-        keni.personInfo();
-        keni.printInput("Baltazar", 21);
+        int[] x ={1,2,3,4,5,6,7,8,9};
+        String[] names = new String[4];
+        names[0] = "Mark";
+        names[2]="Anthony";
+        System.out.println(names[0] + names[2]);
 
-        int x = 3;
-        System.out.println(x);
+        for(int i =0; i <= names.length; i++){
+            System.out.println(names[i]);
 
-        Bag thisBag = new Bag();
-        System.out.println(thisBag.ballpen);
+        }
     }
 }
